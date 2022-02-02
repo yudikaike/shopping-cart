e20cf877de4f8b1bb777dc1b8a060663b520ca02
Kaike Yudi <kaikeyudi@outlook.com>
2022-02-02 17:45:13 -0300

    requisito 3

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.currentTarget.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
